127062bb227def6e3f857d286e969534ff4d76e4
gina3-uk <84281092+gina3-uk@users.noreply.github.com>
2021-08-28 12:06:37 +0100

    Added forecast API call to get daily forecast data

--- src/app.js
+++ src/app.js
@@ -87,6 +87,14 @@ function showWeatherData(response) {
   celsiusTemperature = response.data.main.temp;
   celsiusTemperatureMax = response.data.main.temp_max;
   celsiusTemperatureMin = response.data.main.temp_min;
+
+  getForecast(response.data.coord);
+}
+
+function getForecast(coordinates) {
+  let apiKey = "ac730adc7d8efa5c2d9bf7cf3f38ab81";
+  let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric&exclude=hourly,minutely`;
+  axios.get(apiURL).then(displayForecast);
 }
 
 function toFahrenheit(event) {
@@ -97,12 +105,10 @@ function toFahrenheit(event) {
   document.querySelector("#currentTemp").innerHTML = Math.round(
     fahrenheitTemperature
   );
-
   let fahrenheitTemperatureMax = (celsiusTemperatureMax * 9) / 5 + 32;
   document.querySelector("#currentLocationHigh").innerHTML = `${Math.round(
     fahrenheitTemperatureMax
   )}°F`;
-
   let fahrenheitTemperatureMin = (celsiusTemperatureMin * 9) / 5 + 32;
   document.querySelector("#currentLocationLow").innerHTML = `${Math.round(
     fahrenheitTemperatureMin
@@ -123,7 +129,8 @@ function toCelsius(event) {
   )}°C`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
 
@@ -158,4 +165,3 @@ celsiusLink.addEventListener("click", toCelsius);
 
 formattedDateTime();
 searchCity("Cambridge,UK");
-displayForecast();
